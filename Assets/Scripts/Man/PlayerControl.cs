c08c8adb90b82958b1005f25affb3b854eae52db
MassterMax <maxmail877@gmail.com>
2024-09-12 00:46:47 +0300

    add trash, bag and can

--- Assets/Scripts/Man/PlayerControl.cs
+++ Assets/Scripts/Man/PlayerControl.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField] float playerSpeed;
-
+    const float PLAYER_SPEED = 4f;
+    float playerSpeed;
 
     Dictionary<KeyCode, Vector2> keyToVector = new Dictionary<KeyCode, Vector2>() {
         { KeyCode.W, Vector2.up},
@@ -25,6 +27,16 @@ public class PlayerControl : MonoBehaviour
     PuddleSpawner puddleSpawner;
 
     GameObject broom;
+    GameObject trashBag;
+    SpriteRenderer trashBagSpriteRenderer;
+    int trashCount = 0;
+
+    float trashBagScale = 1.1f;
+    float playerSpeedScale = 0.85f;
+
+    Dictionary<Vector3, Collider2D> collidedJunk = new Dictionary<Vector3, Collider2D>();
+
+    [SerializeField] GameObject trashCan; // move to ok solution
 
     // Start is called before the first frame update
     void Start()
@@ -33,8 +45,22 @@ public class PlayerControl : MonoBehaviour
         animator = GetComponent<Animator>();
         puddleSpawner = FindObjectOfType<PuddleSpawner>();
         puddleSpawner.SetPlayer(this);
-        broom = transform.GetChild(0).gameObject;
-        broom.SetActive(false);
+
+        foreach (Transform child in GetComponentInChildren<Transform>())
+        {
+            if (child.name == "broom")
+            {
+                broom = child.gameObject;
+                broom.SetActive(false);
+            }
+            else if (child.name == "trash-bag")
+            {
+                trashBag = child.gameObject;
+                trashBagSpriteRenderer = trashBag.GetComponent<SpriteRenderer>();
+            }
+        }
+
+        ClearTrashCount();
     }
 
     // Update is called once per frame
@@ -44,6 +70,7 @@ public class PlayerControl : MonoBehaviour
         Rotate();
         HandleRunningAnimation();
         HandlePuddleWashing();
+        HandleJunk();
     }
 
     void Move()
@@ -53,7 +80,8 @@ public class PlayerControl : MonoBehaviour
         direction = Vector2.zero;
 
         foreach (var pair in keyToVector)
-            if (Input.GetKey(pair.Key)) {
+            if (Input.GetKey(pair.Key))
+            {
                 direction += pair.Value;
             }
 
@@ -75,6 +103,8 @@ public class PlayerControl : MonoBehaviour
         if (playerSpriteRenderer.flipX != Mathf.Sign(direction.x) < 0)
         {
             playerSpriteRenderer.flipX = !playerSpriteRenderer.flipX;
+            trashBagSpriteRenderer.flipX = playerSpriteRenderer.flipX;
+            trashBag.transform.localPosition = new Vector3(-trashBag.transform.localPosition.x, trashBag.transform.localPosition.y, trashBag.transform.localPosition.z);
         }
     }
 
@@ -87,22 +117,96 @@ public class PlayerControl : MonoBehaviour
     {
         if (isWashing) return;
 
-        if (puddleSpawner.NearPuddle()) {
+        if (puddleSpawner.NearPuddle())
+        {
             broom.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.Space) && puddleSpawner.TryToWashPuddle()) {
+            if (Input.GetKeyDown(KeyCode.Space) && puddleSpawner.TryToWashPuddle())
+            {
                 broom.SetActive(false);
                 isWashing = true;
                 animator.SetBool("isWashing", isWashing);
             }
-        } else {
+        }
+        else
+        {
             broom.SetActive(false);
         }
     }
 
-    public void StopWashing() 
+    public void StopWashing()
     {
         isWashing = false;
         animator.SetBool("isWashing", isWashing);
     }
 
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "junk")
+        {
+            if (collidedJunk.ContainsKey(collider.transform.position))
+            {
+                throw new System.Exception();
+            }
+            collidedJunk.Add(collider.transform.position, collider);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "junk")
+        {
+            collidedJunk.Remove(collider.transform.position);
+        }
+    }
+
+    void HandleJunk()
+    {
+        if (isWashing) return;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (collidedJunk.Count > 0)
+            {
+                Debug.Log("inside HandleJunk: try to destory first junk");
+                float distance = -1;
+                Vector3 preferedJunk = Vector3.zero;
+
+                foreach (Vector3 pos in collidedJunk.Keys)
+                {
+                    if (distance == -1 || distance > (pos - transform.position).sqrMagnitude)
+                    {
+                        distance = (pos - transform.position).sqrMagnitude;
+                        preferedJunk = pos;
+                    }
+                }
+
+                GameObject junk = collidedJunk[preferedJunk].gameObject;
+                collidedJunk.Remove(preferedJunk);
+                Destroy(junk);
+                IncreaseTrashCount();
+            }
+            else if ((trashCan.transform.position - transform.position).sqrMagnitude < 0.5 * 0.5)
+            {
+                Debug.Log("inside HandleJunk: try to drop junk to can");
+                ClearTrashCount();
+            }
+        }
+    }
+
+    void IncreaseTrashCount()
+    {
+        trashCount += 1;
+        trashBag.transform.localScale *= trashBagScale;
+        playerSpeed *= playerSpeedScale;
+        trashBag.SetActive(true);
+    }
+
+    void ClearTrashCount()
+    {
+        trashCount = 0;
+        trashBag.transform.localScale = Vector3.one;
+        playerSpeed = PLAYER_SPEED;
+        trashBag.SetActive(false);
+    }
 }
